737ec77a813aced50641a0cc7d09615b381468a8
Marcus Bournonwille <Mbouornonwille@gmail.com>
2021-12-09 13:02:08 +0100

    dumme mig

---
+++ AWebsite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AWebsite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
