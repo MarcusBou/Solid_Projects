5b65ce17be8ee42bf0e6b979564b67d6342cd9ab
Marcus Bournonwille <Mbouornonwille@gmail.com>
2021-12-09 09:29:44 +0100

    Også med lone

---
+++ Help_Lone/Interfaces/ISwordFight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Help_Lone
+{
+    interface ISwordFight
+    {
+        public void Bash();
+        public void Cleave();
+        public void Slash();
+    }
+}
---
+++ Help_Lone/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Help_Lone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
